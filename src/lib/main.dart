1ca90755da616e376f0290d6113c9e6e3baaf5e1
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2021-12-09 14:00:09 +0100

    Implementado todo.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -26,68 +26,62 @@ hay que cambiar en la url de la petición localhost:8080 por 10.0.2.2:8080
 
 //{"id":130,"max_capacity":60,"name":"Centro comercial Patricia Cano","address":"1238 Calle de Arturo Soria","percentage_capacity_allowed":40}
 
-/*
-class DebugPage extends StatelessWidget {
-  const DebugPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Debug'),
-        ),
-        body: TextFormField(
-          decoration: InputDecoration(
-              hintText: "x.x.x.x", labelText: "Introduzca la IP de la BBDD"),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter some text';
-            }
-          },
-        )
-    );
-  }
-}*/
-
 // MODELO
+class Modelo {
+  static String ip = "10.0.2.2";
 
-Future<List> getFacilities() async{
-  const ipMovil = "10.0.2.2";
-  var url = Uri.parse("http://$ipMovil:8080/api/rest/facilities");
-  var request = http.Request("GET", url);
-  request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
-  var client = http.Client();
-  var streamedResponse = await client.send(request);
-  var dataAsString = await streamedResponse.stream.bytesToString();
-  client.close();
-  await Future.delayed(Duration(seconds: 2));
-  var dataAsMap = json.decode(dataAsString) as Map;
-  return dataAsMap["facilities"];
-}
+  static Future<List> getFacilities() async {
+    var url = Uri.parse("http://$ip:8080/api/rest/facilities");
+    var request = http.Request("GET", url);
+    request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
+    var client = http.Client();
+    var streamedResponse = await client.send(request);
+    var dataAsString = await streamedResponse.stream.bytesToString();
+    client.close();
+    await Future.delayed(Duration(seconds: 2));
+    var dataAsMap = json.decode(dataAsString) as Map;
+    return dataAsMap["facilities"];
+  }
 
-Future<List> getEvent(int facility_id ,DateTime desde, DateTime hasta) async{
-  const ipMovil = "10.0.2.2";
-  var url = Uri.parse("http://$ipMovil:8080/api/rest/facility_access_log/$facility_id/daterange");
-  var request = http.Request("GET", url);
-  request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
-  request.body = '{ "startdate": "${desde.toIso8601String()}+00:000", "enddate": "${hasta.toIso8601String()}+00:000"}';
-  var client = http.Client();
-  var streamedResponse = await client.send(request);
-  var dataAsString = await streamedResponse.stream.bytesToString();
-  client.close();
-  await Future.delayed(Duration(seconds: 2));
-  var dataAsMap = json.decode(dataAsString) as Map;
-  return dataAsMap["access_log"];
-}
-
-Future<List> getUsers(String name, String surname) async {
-  const ipMovil = "10.0.2.2";
-  name = name.trim();
-  surname = surname.trim();
-
-  if (!name.isEmpty && !surname.isEmpty) {
+  static Future<List> getEvent(int facility_id, DateTime desde, DateTime hasta) async {
     var url = Uri.parse(
-        "http://$ipMovil:8080/api/rest/user?name=$name&surname=$surname");
+        "http://$ip:8080/api/rest/facility_access_log/$facility_id/daterange");
+    var request = http.Request("GET", url);
+    request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
+    request.body =
+    '{ "startdate": "${desde.toIso8601String()}+00:000", "enddate": "${hasta
+        .toIso8601String()}+00:000"}';
+    var client = http.Client();
+    var streamedResponse = await client.send(request);
+    var dataAsString = await streamedResponse.stream.bytesToString();
+    client.close();
+    await Future.delayed(Duration(seconds: 2));
+    var dataAsMap = json.decode(dataAsString) as Map;
+    return dataAsMap["access_log"];
+  }
+
+  static Future<List> getUsers(String name, String surname) async {
+    name = name.trim();
+    surname = surname.trim();
+
+    if (!name.isEmpty && !surname.isEmpty) {
+      var url = Uri.parse(
+          "http://$ip:8080/api/rest/user?name=$name&surname=$surname");
+      var request = http.Request("GET", url);
+      request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
+      var client = http.Client();
+      var streamedResponse = await client.send(request);
+      var dataAsString = await streamedResponse.stream.bytesToString();
+      client.close();
+      var dataAsMap = json.decode(dataAsString) as Map;
+      List users = dataAsMap["users"];
+
+      if (!users.isEmpty) {
+        return users;
+      }
+    }
+
+    var url = Uri.parse("http://$ip:8080/api/rest/users");
     var request = http.Request("GET", url);
     request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
     var client = http.Client();
@@ -97,86 +91,74 @@ Future<List> getUsers(String name, String surname) async {
     var dataAsMap = json.decode(dataAsString) as Map;
     List users = dataAsMap["users"];
 
-    if (!users.isEmpty) {
-      return users;
-    }
-  }
+    int i = 0;
+    Map user = users[i];
 
-  var url = Uri.parse("http://$ipMovil:8080/api/rest/users");
-  var request = http.Request("GET", url);
-  request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
-  var client = http.Client();
-  var streamedResponse = await client.send(request);
-  var dataAsString = await streamedResponse.stream.bytesToString();
-  client.close();
-  var dataAsMap = json.decode(dataAsString) as Map;
-  List users = dataAsMap["users"];
-
-  int i = 0;
-  Map user = users[i];
-
-  if (!name.isEmpty) {
-    while (i < users.length) {
-      user = users[i];
-      print(user["name"]);
-      if ((user["name"] as String).toUpperCase()
-          .startsWith(name.toUpperCase())) {
-        i++;
-      } else {
-        users.removeAt(i);
-        if (i != 0) {
-          i--;
+    if (!name.isEmpty) {
+      while (i < users.length) {
+        user = users[i];
+        print(user["name"]);
+        if ((user["name"] as String).toUpperCase()
+            .startsWith(name.toUpperCase())) {
+          i++;
+        } else {
+          users.removeAt(i);
+          if (i != 0) {
+            i--;
+          }
         }
       }
     }
-  }
-  i = 0;
-  if (!surname.isEmpty) {
-    while (i < users.length) {
-      user = users[i];
-      if ((user["surname"] as String).toUpperCase()
-          .startsWith(surname.toUpperCase())) {
-        i++;
-      } else {
-        users.removeAt(i);
-        if (i != 0) {
-          i--;
+    i = 0;
+    if (!surname.isEmpty) {
+      while (i < users.length) {
+        user = users[i];
+        if ((user["surname"] as String).toUpperCase()
+            .startsWith(surname.toUpperCase())) {
+          i++;
+        } else {
+          users.removeAt(i);
+          if (i != 0) {
+            i--;
+          }
         }
       }
     }
+
+    return users;
   }
 
-  return users;
+  static Future<Map> postAccLog(int facility_id, String user_id,
+      DateTime timestamp, String type, double temperature) async {
+    var url = Uri.parse("http://$ip:8080/api/rest/access_log");
+    var request = http.Request("POST", url);
+    request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
+    request.body =
+    '{'
+        '"facility_id": $facility_id,'
+        '"user_id": "$user_id",'
+        '"timestamp": "${timestamp.toIso8601String()}",'
+        '"type": "$type",'
+        '"temperature": "${temperature.toString()}"'
+        '}';
+    var client = http.Client();
+    var streamedResponse = await client.send(request);
+    var dataAsString = await streamedResponse.stream.bytesToString();
+    client.close();
+    await Future.delayed(Duration(seconds: 2));
+    var dataAsMap = json.decode(dataAsString) as Map;
+    return dataAsMap["insert_access_log_one"];
+  }
 }
-
-Future<Map> postAccLog(int facility_id, String user_id,
-    DateTime timestamp, String type, double temperature) async {
-  const ipMovil = "10.0.2.2";
-  var url = Uri.parse("http://$ipMovil:8080/api/rest/access_log");
-  var request = http.Request("POST", url);
-  request.headers.addAll({"x-hasura-admin-secret": "myadminsecretkey"});
-  request.body =
-  '{'
-      '"facility_id": $facility_id,'
-      '"user_id": "$user_id",'
-      '"timestamp": "${timestamp.toIso8601String()}",'
-      '"type": "$type",'
-      '"temperature": "${temperature.toString()}"'
-  '}';
-  var client = http.Client();
-  var streamedResponse = await client.send(request);
-  var dataAsString = await streamedResponse.stream.bytesToString();
-  client.close();
-  await Future.delayed(Duration(seconds: 2));
-  var dataAsMap = json.decode(dataAsString) as Map;
-  return dataAsMap["insert_access_log_one"];
-}
-
 String dateToString(DateTime fecha) =>
     fecha.toIso8601String().substring(0, 10);
 
 String timeToString(DateTime fecha) =>
     fecha.toString().substring(11, 16);
+
+enum Idiomas { esp, eng }
+
+// VISTA
 
 class Alert extends StatelessWidget {
   final String _text;
@@ -214,9 +196,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-// VISTA
-
 class FacilitiesPage extends StatefulWidget {
   const FacilitiesPage({Key? key}) : super(key: key);
 
@@ -230,7 +209,7 @@ class _FacilitiesPageState extends State<FacilitiesPage> {
   @override
   initState() {
     super.initState();
-    _value = getFacilities();
+    _value = Modelo.getFacilities();
   }
 
   @override
@@ -239,6 +218,28 @@ class _FacilitiesPageState extends State<FacilitiesPage> {
         appBar: AppBar(
           title: const Text("Centros"),
         ),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.sync),
+              iconSize: 20,
+              tooltip: 'Recargar',
+              onPressed: () => setState(() {
+                _value = Modelo.getFacilities();
+              })
+          ),
+          IconButton(
+              icon: const Icon(Icons.settings),
+              iconSize: 20,
+              tooltip: 'Configuración',
+              onPressed: () =>
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ConfiguracionPage()
+                      )
+                  )
+          ),
+        ],
         body: Center(child: Column(
             children: <Widget>[
               Container(
@@ -306,6 +307,69 @@ class _FacilitiesPageState extends State<FacilitiesPage> {
   }
 }
 
+class ConfiguracionPage extends StatefulWidget {
+  ConfiguracionPage({Key? key}): super(key: key);
+
+  @override
+  _ConfigurationPageState createState() => _ConfigurationPageState();
+
+}
+
+class _ConfigurationPageState extends State<StatefulWidget> {
+  late String ipBBDD;
+  late Idiomas idioma;
+
+  @override
+  void initState() {
+    super.initState();
+    ipBBDD = Modelo.ip;
+    idioma = Idiomas.esp;
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Configuración'),
+        ),
+        body: TextFormField(
+          decoration: InputDecoration(
+              hintText: ipBBDD, labelText: "Introduzca la IP de la BBDD"),
+          keyboardType: TextInputType.number,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (value) {
+
+            if(value == null){
+              return "Formato incorrecto";
+            }
+            value = value as String;
+            var values = value.split('.');
+            if(values.length < 4){
+              return "Formato incorrecto";
+            }
+            for(String i in values) {
+              if(i.isEmpty || i.toUpperCase() != i.toLowerCase()){
+                return "Formato incorrecto";
+              }
+            }
+          },
+          onFieldSubmitted: (value) {
+            if(value == null) return;
+            value = value.trim();
+            if (!value.isEmpty) {
+              setState(() {
+                ipBBDD = value;
+                Modelo.ip = ipBBDD;
+              });
+            }
+          },
+        )
+    );
+  }
+}
+
 class OpcionesPage extends StatelessWidget {
   Map facility;
 
@@ -352,56 +416,81 @@ class OpcionesPage extends StatelessWidget {
   }
 }
 
-class IdentificarPage extends StatelessWidget {
+class IdentificarPage extends StatefulWidget {
   Map facility;
 
-  IdentificarPage({Key? key, required this.facility}): super(key: key);
+  IdentificarPage({Key? key, required this.facility}) : super(key: key);
+
+  @override
+  _IdentificarPageState createState() => _IdentificarPageState();
+
+}
+
+class _IdentificarPageState extends State<IdentificarPage> {
+  late Future<String> _futQr;
+
+  @override
+  void initState() {
+    super.initState();
+    _futQr = Future(() => '-1');
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Identificar usuario')),
-      body: Builder(builder: (BuildContext context) {
-        return Container(
-          alignment: Alignment.center,
-          child: Flex(
-            direction: Axis.vertical,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () => FlutterBarcodeScanner.scanBarcode(
-                  '#ff6666', 'Cancel', true, ScanMode.QR).then(
-                        (value) {
-                    if(value != '-1') {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) =>
-                            RegistrarPage(uuid: value, facility: facility,)
-                        ),
-                      );
-                    }
-                  }
+        appBar: AppBar(title: const Text('Identificar usuario')),
+        body: Builder(builder: (BuildContext context)
+    {
+      return FutureBuilder(
+          future: _futQr,
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData &&
+                snapshot.data != null &&
+                snapshot.data != '-1') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        RegistrarPage(uuid: snapshot.data as String,
+                          facility: widget.facility,)
                 ),
-                child: const Text('Con QR',
-                    textScaleFactor: 1.5
+              );
+            }
+            return Container(
+                alignment: Alignment.center,
+                child: Flex(
+                    direction: Axis.vertical,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                          onPressed: () =>
+                              setState(() =>
+                              _futQr = FlutterBarcodeScanner.scanBarcode(
+                                  '#ff6666', 'Cancel', true, ScanMode.QR)
+                              ),
+                          child: const Text('Con QR',
+                              textScaleFactor: 1.5
+                          )
+                      ),
+                      ElevatedButton(
+                          onPressed: () =>
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) =>
+                                      UsuariosPage(
+                                          facility: widget.facility))
+                              ),
+                          child: const Text('Manual',
+                              textScaleFactor: 1.5
+                          )
+                      ),
+                    ]
                 )
-              ),
-              ElevatedButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>
-                    UsuariosPage(facility: facility))
-                ),
-                child: const Text('Manual',
-                    textScaleFactor: 1.5
-                )
-              ),
-            ]
-          )
-        );
-      }
-      )
-    );
+            );
+          }
+      );
+    }));
   }
 }
 
@@ -422,7 +511,7 @@ class _UsuariosPageState extends State<UsuariosPage> {
   @override
   initState() {
     super.initState();
-    _value = getUsers(_nombre, _apellido);
+    _value = Modelo.getUsers(_nombre, _apellido);
   }
 
   @override
@@ -447,7 +536,7 @@ class _UsuariosPageState extends State<UsuariosPage> {
                       if (values.length > 1) {
                         _apellido = values[1];
                       }
-                        _value = getUsers(_nombre, _apellido);
+                        _value = Modelo.getUsers(_nombre, _apellido);
                     });
                   }
                 },
@@ -540,7 +629,7 @@ class _EventPageState extends State<EventPage> {
     fechaDesde = DateTime(year, month, day,
         fechaHasta.hour, fechaHasta.minute);
 
-    _futAccLog = getEvent(widget.facility['id'], fechaDesde, fechaHasta);
+    _futAccLog = Modelo.getEvent(widget.facility['id'], fechaDesde, fechaHasta);
 
     _futFechaDesde = Future(() => fechaDesde);
     _futFechaHasta = Future(() => fechaHasta);
@@ -755,7 +844,7 @@ class _EventPageState extends State<EventPage> {
                             iconSize: 25,
                             tooltip: 'Austar la fecha de inicio del evento',
                             onPressed: () => setState(() {
-                              _futAccLog = getEvent(widget.facility['id'],
+                              _futAccLog = Modelo.getEvent(widget.facility['id'],
                                   fechaDesde, fechaHasta);
                             })
                         ),
@@ -1031,7 +1120,7 @@ class _RegistrarPageState extends State<RegistrarPage> {
                 iconSize: 50,
                 tooltip: 'Austar la fecha de inicio del evento',
                 onPressed: () => setState(() {
-                  _futAcc = postAccLog(
+                  _futAcc = Modelo.postAccLog(
                       widget.facility['id'],
                       widget.uuid,
                       fecha,
